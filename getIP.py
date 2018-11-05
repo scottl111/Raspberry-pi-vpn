be76cc3964d2d2bbd0887b6fcae53a28e2e5b32e
Scott <Scott>
2018-11-05 00:21:52 +0000

    First commit. Querying the machines IP address

---
+++ getIP.py
@@ -0,0 +1,19 @@
+import requests
+
+class IpQuery:
+
+	def getIpAddress(self):
+		# Query the endpoint to grab the JSON, parse the IP Address and return it.
+		endpoint = requests.get('https://ifconfig.co/ip')  
+		ipAddress = endpoint.text
+		return ipAddress
+		
+	def getApiUrl(self):
+		# TODO
+		
+def main():
+	ipQuery = IpQuery()
+	print(ipQuery.getIpAddress())
+	
+if __name__ == '__main__':
+	main()
